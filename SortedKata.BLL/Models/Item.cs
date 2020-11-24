03816121c0b9a3859453f5e0bea366cc4649f5b7
JAV <javkhalid@yahoo.co.uk>
2020-11-24 07:52:57 +0000

    Test, and BLL project added to the solution. Model class created.

---
+++ SortedKata.BLL/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortedKata.BLL.Models
+{
+    public class Item
+    {
+        public string SKU { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ SortedKata.BLL/Models/ItemOffer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortedKata.BLL.Models
+{
+    public class ItemOffer
+    {
+        public string SKU { get; set; }
+        public int Quantity { get; set; }
+        public decimal OfferPrice { get; set; }
+    }
+}
